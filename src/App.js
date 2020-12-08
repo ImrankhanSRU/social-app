a9b9846eb1c0e71adb61863734340b84811fef6d
Imran <imran.khan@mountblue.io>
2020-12-08 11:50:07 +0530

    Login created.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/login/Login'
+import NewsFeed from './components/news-feed/NewsFeed'
+
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Redirect exact from="/" to="login" />
+      <Route path="/login" component={Login} />
+      <Route path="/newsfeed" component={NewsFeed} />
+
+    </Router>
   );
 }
 
---
+++ src/components/login/Login.js
@@ -0,0 +1,23 @@
+import './Login.css';
+import { useState } from 'react'
+import { useHistory } from "react-router-dom";
+
+export default function Login() {
+    const history = useHistory();
+
+    let [userName, setUserName] = useState('')
+    let [password, setpassword] = useState('')
+    return (
+        <div className="login-container">
+            <div className="header">
+                Login
+        </div>
+            <input placeholder="User Name" value={userName} onChange={(e) => { setUserName(e.target.value) }} />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => { setpassword(e.target.value) }} />
+            <button className="primary-btn" disabled={!(userName.length && password.length)}
+                onClick={() => history.push('/newsfeed')}>
+                Login
+            </button>
+        </div>
+    );
+}
